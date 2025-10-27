01c2f37a218f342e1edcd0be3075b057e46d40ae
1Momori1 <165267189+1Momori1@users.noreply.github.com>
2025-10-27 12:13:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 # Получаем токен из переменных окружения (Railway) или используем локальный
-API_TOKEN = '7684724135:AAEWhZ1vf9F5yFh__jKMA8ljnQ0ReojAEIU'
+API_TOKEN = '7'
 DB_PATH = 'bots.db'
 
 # Список Telegram ID, которым разрешён доступ к менеджеру
-WHITE_LIST_IDS = [824312416, 1094229384]  # Хамраев Амирхан (@Blet_pit), Светлана Кузмичева (@FatiniA_ks) 
+WHITE_LIST_IDS = 
